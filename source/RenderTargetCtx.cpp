4dfd48a7392051d54b695ad293fa12f81ddd332d
zz <zhuguang@ejoy.com>
2019-04-07 10:10:35 +0800

    [FIXED] update ctx

--- source/RenderTargetCtx.cpp
+++ source/RenderTargetCtx.cpp
@@ -1,6 +1,9 @@
 #include "painting2/RenderTargetCtx.h"
 #include "painting2/Shader.h"
 
+#include "painting2/Blackboard.h"
+#include "painting2/WindowContext.h"
+
 #include <unirender/RenderContext.h>
 
 namespace pt2
@@ -14,14 +17,21 @@ RenderTargetCtx::RenderTargetCtx(ur::RenderContext& rc, const std::shared_ptr<Sh
     m_rc.GetViewport(m_vp_x, m_vp_y, m_vp_w, m_vp_h);
     m_rc.SetViewport(0, 0, width, height);
 
-    m_shader->UpdateModelMat(sm::mat4());
-    m_shader->UpdateProjMat(width, height);
-    m_shader->UpdateViewMat(sm::vec2(), 1);
+    if (m_shader) {
+        m_shader->UpdateModelMat(sm::mat4());
+        m_shader->UpdateProjMat(width, height);
+        m_shader->UpdateViewMat(sm::vec2(), 1);
+    }
 }
 
 RenderTargetCtx::~RenderTargetCtx()
 {
-    m_shader->UpdateProjMat(m_vp_w, m_vp_h);
+    if (m_shader) {
+        // fixme
+        auto wc = Blackboard::Instance()->GetWindowContext();
+        wc->Bind();
+//        m_shader->UpdateProjMat(m_vp_w, m_vp_h);
+    }
     m_rc.SetViewport(m_vp_x, m_vp_y, m_vp_w, m_vp_h);
 }
 
