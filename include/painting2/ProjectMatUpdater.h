aaf26879c90b3677f4de8623ef8f1d5a33abe239
zz <xzrunner@gmail.com>
2020-04-22 07:57:05 +0800

    [ADDED] Snapshot for uniform update

--- include/painting2/ProjectMatUpdater.h
+++ include/painting2/ProjectMatUpdater.h
@@ -25,11 +25,32 @@ public:
 
     void Update(float width, float height);
 
+public:
+    class Snapshot
+    {
+    public:
+        Snapshot(ProjectMatUpdater& updater)
+            : m_updater(updater), m_width(updater.m_width), m_height(updater.m_height) {}
+
+        void Restore() {
+            m_updater.Update(m_width, m_height);
+        }
+
+    private:
+        ProjectMatUpdater& m_updater;
+
+        float m_width = 0;
+        float m_height = 0;
+
+    }; // Snapshot
+
 private:
     std::shared_ptr<ur2::Uniform> m_uniform = nullptr;
 
     float m_width  = 0;
     float m_height = 0;
+
+    friend class Snapshot;
 
 }; // ProjectMatUpdater
 
--- include/painting2/ViewMatUpdater.h
+++ include/painting2/ViewMatUpdater.h
@@ -26,11 +26,32 @@ public:
 
     void Update(const sm::vec2& offset, float scale);
 
+public:
+    class Snapshot
+    {
+    public:
+        Snapshot(ViewMatUpdater& updater)
+            : m_updater(updater), m_offset(updater.m_view_offset), m_scale(updater.m_view_scale) {}
+
+        void Restore() {
+            m_updater.Update(m_offset, m_scale);
+        }
+
+    private:
+        ViewMatUpdater& m_updater;
+
+        sm::vec2 m_offset;
+        float    m_scale = 0;
+
+    }; // Snapshot
+
 private:
     std::shared_ptr<ur2::Uniform> m_uniform = nullptr;
 
     sm::vec2 m_view_offset;
     float    m_view_scale = 0;
+
+    friend class Snapshot;
 
 }; // ViewMatUpdater
 
